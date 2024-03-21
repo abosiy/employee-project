3c7c366bc19ed353fc9bc3a6dc828677a4790ddf
abosiy1996 <abosiy1996@gmail.com>
2024-03-21 20:32:03 +0200

    [FEED] - fixed comments.

--- src/com/bosiy/Employee.java
+++ src/com/bosiy/Employee.java
@@ -1,7 +1,6 @@
 package com.bosiy;
 
 public class Employee {
-
     private String name;
     private  double salary;
     private  String position;
@@ -15,18 +14,23 @@ public class Employee {
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public double getSalary() {
         return salary;
     }
+
     public void setSalary(double salary) {
         this.salary = salary;
     }
+
     public String getPosition() {
         return position;
     }
+
     public void setPosition(String position) {
         this.position = position;
     }
